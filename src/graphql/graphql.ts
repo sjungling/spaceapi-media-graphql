f1fd75f7901d8e8508d054fc288ef1698a59d4d8
Scott Jungling <scott.jungling@build.com>
2020-12-12 15:24:04 -0800

    feat: functioning serverless apollo instance

---
+++ src/graphql/graphql.ts
@@ -0,0 +1,18 @@
+import { ApolloServer, makeExecutableSchema } from "apollo-server-lambda";
+import NasaMediaAPI from "./nasa-media-data";
+import { NasaMediaResolvers } from "./nasa-media-resolver";
+import NasaMediaSchema from "./nasa-media-schema.graphql";
+
+const schemaWithResolvers = makeExecutableSchema({
+  typeDefs: NasaMediaSchema,
+  resolvers: NasaMediaResolvers,
+});
+
+const server = new ApolloServer({
+  schema: schemaWithResolvers,
+  dataSources: () => ({
+    nasa: new NasaMediaAPI(),
+  }),
+});
+
+export const handler = server.createHandler();
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+const nodeExternals = require("webpack-node-externals");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  externals: [nodeExternals()],
+  optimization: { minimize: false },
+  mode: "development",
+  node: {
+    global: false,
+    __filename: false,
+    __dirname: true,
+  },
+  plugins: [
+    new CleanWebpackPlugin({ verbose: true }),
+    new CopyPlugin({
+      patterns: [{ from: "**/*.graphql", to: "[name].[ext]" }],
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.graphql$/,
+        exclude: /node_modules/,
+        loader: "@graphql-tools/webpack-loader",
+      },
+    ],
+  },
+};
