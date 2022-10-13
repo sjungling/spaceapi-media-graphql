579648b9826b6611dc7f196efc82e020072fd3bc
Scott Jungling <scott@moderne.io>
2022-10-12 20:17:46 -0700

    fix: resolve outdated as-lambda

--- src/graphql/graphql.ts
+++ src/graphql/graphql.ts
@@ -1,11 +1,10 @@
 import { ApolloServer } from "apollo-server-lambda";
-import { buildFederatedSchema } from "@apollo/federation";
-import { ApolloServerPluginUsageReportingDisabled } from "apollo-server-core";
+import { buildSubgraphSchema } from "@apollo/federation";
 import NasaMediaAPI from "./nasa-media-data";
 import { NasaMediaResolvers } from "./nasa-media-resolver";
 import NasaMediaSchema from "./nasa-media-schema.graphql";
 
-const schemaWithResolvers = buildFederatedSchema({
+const schemaWithResolvers = buildSubgraphSchema({
   typeDefs: NasaMediaSchema,
   resolvers: NasaMediaResolvers,
 });
@@ -15,19 +14,15 @@ const server = new ApolloServer({
   dataSources: () => ({
     nasa: new NasaMediaAPI(),
   }),
-  playground: true,
-  introspection: true,
-  cacheControl: {
-    defaultMaxAge: 60 * 60,
-  },
-  plugins: [ApolloServerPluginUsageReportingDisabled()],
 });
 
 export const handler = server.createHandler({
-  cors: {
-    origin: "*",
-    methods: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
-    allowedHeaders: "*",
-    credentials: true,
+  expressGetMiddlewareOptions: {
+    cors: {
+      origin: "*",
+      methods: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+      allowedHeaders: "*",
+      credentials: true,
+    },
   },
 });
